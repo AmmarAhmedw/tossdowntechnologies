708e509994033cdda21dd39263ab51f807fd8f59
Ammar Ahmed <hamza.ahmed960@yahoo.com>
2025-07-21 02:38:30 +0500

    Fix Tailwind CSS configuration - downgrade to v3 and fix PostCSS setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import { heroui } from "@heroui/react";
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
@@ -30,33 +28,4 @@ export default {
 		},
 	},
 	darkMode: "class",
-	plugins: [
-		heroui({
-			themes: {
-				light: {
-					colors: {
-						primary: {
-							50: '#fdf2f8',
-							100: '#fce7f3',
-							200: '#fbcfe8',
-							300: '#f9a8d4',
-							400: '#f472b6',
-							500: '#ec4899',
-							600: '#db2777',
-							700: '#be185d',
-							800: '#9d174d',
-							900: '#831843',
-							DEFAULT: '#ec4899',
-						},
-						background: {
-							DEFAULT: '#ffffff',
-						},
-						foreground: {
-							DEFAULT: '#1f2937',
-						}
-					}
-				}
-			}
-		})
-	],
 };
